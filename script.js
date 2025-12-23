f465ded6e94cb9c00b1644eb8958a420e4235d31
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-23 14:04:37 +0600

    include tag

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+const plus = document.getElementById('plus');
+const reset = document.getElementById('reset');
+const minus = document.getElementById('minus');
+
+
+//  ========= 2nd Step ==========
+let count = 0;
+
+plus.addEventListener('click', () => {
+
+    count = count + 1
+    count.textContent = innerText
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const updateCounter = (value) => {
+
+
+//     count = count + value
+//     createElement.textContenr = count
+
+
+
+// }
+
+// plus.addEventListener('click', () => {
+//     updateCounter(1);
+// });
+
+// minus.addEventListener('click' , () =>{
+//     updateCounter(-1)
+// })
