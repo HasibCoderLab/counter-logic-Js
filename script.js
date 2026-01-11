13bcc9f25969989d105452959362606fdf8a2af7
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-11 21:41:42 +0600

    set a clock

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const plus = document.getElementById('plus');
 const reset = document.getElementById('reset');
 const minus = document.getElementById('minus');
 const counterElement = document.getElementById('counter');
+const clockElement = document.getElementById("clock");
+
+// ========= Extra =============
+
+setInterval(() => {
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+    clockElement.textContent = time;
+}, 1000);
 
 // =========== 2nd Step ==========
 let count = 0;
