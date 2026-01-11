eaa4c7419de2527b731747b3f10909e484434403
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-11 18:36:13 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// ======== 1st  Step ==========
+const plus = document.getElementById('plus');
+const reset = document.getElementById('reset');
+const minus = document.getElementById('minus');
+const counterElement = document.getElementById('counter');
+
+// =========== 2nd Step ==========
+let count = 0;
+
+
+//  ========= 3rd Step ==========
+
+plus.addEventListener('click', () => {
+    count = count + 1;
+    counterElement.textContent =count;
+});
+
+minus.addEventListener('click', () => {
+    count = count - 1;
+    counterElement.textContent =count;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const updateCounter = (value) => {
+
+
+//     count = count + value
+//     createElement.textContenr = count
+
+
+
+// }
+
+// plus.addEventListener('click', () => {
+//     updateCounter(1);
+// });
+
+// minus.addEventListener('click' , () =>{
+//     updateCounter(-1)
+// })
